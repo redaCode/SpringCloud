83e82a9e58439057ab948ceb3672be334a62cf62
honggui <honggui.shao@aligncloud.com.cn>
2018-01-09 17:34:35 +0800

    第一次提交

---
+++ src/main/java/com/reda/App.java
@@ -0,0 +1,13 @@
+package com.reda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
